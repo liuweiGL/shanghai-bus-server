bab52dcf4a3b008bcf4c1fa01d95ec3cf92dd3a0
liuwei <liuwei89757@gamil.com>
2018-07-31 23:15:25 +0800

    完成基础API

--- apis/bus.js
+++ apis/bus.js
@@ -3,33 +3,74 @@ const axios = require('axios')
 const AMAP_KEY = '78cdb87ae7330aa8949a2c2c9868e834'
 
 /**
- * 根据路线查询公交
- * @param {String} line 公交路线
+ * 查询附近的公交
+ * @param {String} location 经纬度
+ * 
+ * see https://lbs.amap.com/api/webservice/guide/api/search
  */
-exports.queryBusByLine = function (line) {
+exports.queryBusByLocation = function(location){
   return axios({
-    url: 'https://restapi.amap.com/v3/bus/linename',
+    url: 'https://restapi.amap.com/v3/place/around',
     method: 'GET',
     params: {
-      city: '上海',
+      location,
       key: AMAP_KEY,
-      keywords: line
+      offset: 20,
+      radius: 500,
+      city: '上海',
+      types: '交通设施服务|150700', 
     }
   })
 }
 
 /**
- * 根据站台查询公交
- * @param {String} station 站台
+ * 根据路线查询公交
+ * @param {String} router 公交路线
  */
-exports.queryBusByStation = function(station){
+exports.queryBusByRouter = function (router) {
   return axios({
-    url: 'https://restapi.amap.com/v3/bus/stopname',
+    url: 'https://www.amap.com/service/poiInfo',
     method: 'GET',
     params: {
+      keywords: router,
+      pagenum: 1,
       city: '上海',
-      key: AMAP_KEY,
-      keywords: station
+      pagesize: 20,
+      utd_sceneid: 1000,
+      query_type: 'TQUERY'
+    }
+  })
+}
+
+/**
+ * 根据路线获取公交ID
+ * @param {String} router 公交路线
+ */
+exports.queryBusIdByRouter = function (router){
+  return axios({
+    url: 'http://shanghaicity.openservice.kankanews.com/public/bus/get',
+    method: 'POST',
+    data: {
+      idnum: router
+    }
+  })
+}
+
+/**
+ * 查询公交到站信息
+ * @param {Object} data
+ * {
+ *   stopid: 站台ID,
+ *   sid: 公交ID
+ * } 
+ */
+exports.queryBusByStation = function (data){
+  return axios({
+    url: 'http://shanghaicity.openservice.kankanews.com/public/bus/Getstop',
+    method:'POST',
+    data: {
+      stoptype: 0,
+      ...data
     }
   })
 }
