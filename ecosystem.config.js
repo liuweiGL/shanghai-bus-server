4eded2bf3783ad9e4eab01fde61fc7e69f7b5e69
liuwei <lliuwei1185032659@gmail.com>
2018-08-02 22:28:17 +0800

    使用PM2启动服务

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    name      : 'shanghai-bus-server',
+    script    : './start.js',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : 'liuweigl.cn',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
