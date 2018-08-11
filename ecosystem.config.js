89cd7c654ed0cf75dbff5fbc23fc17b1fab49510
liuwei <lliuwei1185032659@gmail.com>
2018-08-11 17:53:38 +0800

    修改pm2启动参数

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,31 @@
 module.exports = {
-  apps : [{
-    name      : 'shanghai-bus-server',
-    script    : './start.js',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production : {
-      NODE_ENV: 'production'
+  apps: [
+    {
+      name: 'shanghai-bus-server',
+      script: './start.js',
+      instances: 2,
+      merge_logs: true,
+      exec_mode: 'cluster',
+      max_memory_restart: '300M',
+      watch: false,
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
     }
-  }],
+  ],
 
-  deploy : {
-    production : {
-      user : 'node',
-      host : 'liuweigl.cn',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  deploy: {
+    production: {
+      user: 'liuweigl',
+      host: 'liuweigl.cn',
+      ref: 'origin/master',
+      repo: 'git@github.com:liuweiGL/shanghai-bus-server.git',
+      path: '/var/www/production',
+      'post-deploy':
+        'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
-};
+}
