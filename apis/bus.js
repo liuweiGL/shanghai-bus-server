2125ddd9d5e1d0afef52041ce4c3cc6ccea48363
liuwei <wei.liu@pcsg.net>
2019-06-13 15:08:38 +0800

    fix(实时到站): 修复阿里云拦截返回 405

--- apis/bus.js
+++ apis/bus.js
@@ -63,22 +63,39 @@ exports.queryStopInfo = function(data) {
   })
 
   const options = {
+    protocol: 'https:',
     hostname: 'shanghaicity.openservice.kankanews.com',
     port: 443,
     path: '/public/bus/Getstop',
     method: 'POST',
     headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+      Host: 'shanghaicity.openservice.kankanews.com',
+      Origin: 'https://shanghaicity.openservice.kankanews.com',
       'Content-Type': 'application/x-www-form-urlencoded',
       'Content-Length': Buffer.byteLength(body),
-      cookie:
-        'aliyungf_tc=AQAAAOIWmxJ6lwgAVJlXZfuSTgEEckBh; acw_tc=2f624a5b15517658295641353e42f25aee7cbc2c57e0d5b8a7c2f8f2fc6b48; souid=wKgBHFx+ETmOYl5cJ1AWAg==; Hm_p1vt_6f69830ae7173059e935b61372431b35=eSgsNFx+EUWZ+AdDBjvoAg==; _ga=GA1.2.1002298288.1551765831; last_search_records=eyJpdiI6IkNySG1md2F6YzBlU0pQVWF0REptMlE9PSIsInZhbHVlIjoiYnFuNFk4ZlV3ek9IT3ZaSUpudDdCU2o4UTZxR0RXQStET3NqYkt0b3hlYz0iLCJtYWMiOiI2MmY1YjNiNGZlYThjOWVhMjA0YmNmZDg3MmJjOTEyZWJlNDdkYzI3MDAwODAzMmZiMTNmOWMwMTY0MzU2YmE1In0%3D; _gat=1; XSRF-TOKEN=eyJpdiI6IlZRb21iN3BTU2dqNFFha05hWmQwVVE9PSIsInZhbHVlIjoibjNPK25PK0tENHI5eDB0OXRMQk8rYnRiNDVyN2hmZHMyekJUY3hEYTVtVmxvNERqS0RmaDllVEtLQ3ZcL3JiRXhlZDl0ckw0YVlORTJVSmo5T3QzTld3PT0iLCJtYWMiOiIzNmFhY2YyY2YyNjdmZjNhYWM3YWM2MzYzMzRmZThiNDI5MDEzMmUzZWM5NzBhZTg0ZTk4MDZiMDEzMmU3M2Q0In0%3D; _session=eyJpdiI6IkpzVjkwWEM1SFwvdytiSjB6UW9MOU5nPT0iLCJ2YWx1ZSI6ImpkbEN3cHI2MGZRNkhlT2I4Vll2TDFBQ2hBSFhVNmhNQkczeE1sK2lqYU5XYjFBQzdJQ0xVbm5TNlJTMDZnc3NBYzkrZ3pWajNTdUljZ0k3VXBCM2ZnPT0iLCJtYWMiOiJjZjU5YTYzNDUzOGU4MGY4ZGZkYzViYTdiNDUxZjZhNTMwNTE5MmZjNWVhNGZjMzg4MjYwMTg5YzA4YWFiMzJjIn0%3D; SERVERID=4f5f69a3e569b9cd359e6a82e64e12fb|1551767173|1551766674'
+      'User-Agent':
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
+      cookie: ''
+      // 'aliyungf_tc=AQAAABOGjUhChwwAViLrdIJ9BW89dsOF; acw_tc=2f624a4415604060168226336e2e9540f7fe8ffeaee65947a1cbe2774601ea; souid=wKgBEV0B5/WqtFz3L6uBAg==; Hm_p1vt_6f69830ae7173059e935b61372431b35=eSgsNF0B6ACa6AdFO5FjAg==; _ga=GA1.2.709638950.1560406015; _gat=1; last_search_records=eyJpdiI6ImxtUWV6RlwvTXBZckdRVUttTXcwb2VRPT0iLCJ2YWx1ZSI6IkdpV1RkNTFEZENZdVA3Z051NkxDQWZ5dERvQU5UcG44Mk9VN2xoemxtRjg9IiwibWFjIjoiMjY0YTcxN2YxYzczNTRiZmUwMmZjZjVjNGU5OTZhMDcwMjI2NTM3MzZiMjM4YzFlMTI5NDNkYjc4NmRiMzcxNCJ9; XSRF-TOKEN=eyJpdiI6InlNajJLd1dhMjVvd3EyZXpMZElnbkE9PSIsInZhbHVlIjoiY1RNZllZSlcyXC9HeEVwUHR4bXcyS0Vvemo3NVc2azM0ZlhpQW1KY054MHlIUWRiVWw4QzhHRjVkT2pidUI2RHRsbTNES3NJMldFWXNSbHlcLzZ0Q1RoQT09IiwibWFjIjoiNmYwZTc4YmI2MWMzMjcwNzkyZDUyOTM4NzY0ZTk3NDFhOGRjNDYwMzFmNDJjNzM1OWNlZDA0NTcxYTk1ZjY5ZiJ9; _session=eyJpdiI6Ik9ITVJOWkhDdnZFeFE4NDNnWStldGc9PSIsInZhbHVlIjoidUwxamFZRG5VbndTeEpoM2ZlOHNxejBpT2dHYW9xR1JLUnVtb2xWOVA3NmtiRGRHMFdHem04ZElaaFwvWHI1QlVLQUdOdTZLQWYzTkpLT2FCcmsraVFRPT0iLCJtYWMiOiJhMDE2NGVkNTcxNjY2YzM5ZGFmNjAwODM1YjE2MjM2OWZjYWMwZGM5MGU4ZTQ2MmJlZTZjMWZmZWY5ZDU2ZDM3In0%3D; SERVERID=4f5f69a3e569b9cd359e6a82e64e12fb|1560406054|1560406016'
     }
   }
 
   return new Promise((resolve, reject) => {
     const req = https.request(options, (res) => {
+      const chunks = []
+      // 设置编码，返回字符串；否则，返回 Buffer
+      res.setEncoding('utf8')
       res.on('data', (d) => {
-        resolve(JSON.parse(d))
+        chunks.push(d)
+      })
+      res.on('end', () => {
+        const result = chunks.join('')
+        try {
+          resolve(JSON.parse(result))
+        } catch (e) {
+          reject(result)
+        }
       })
     })
     req.write(body)
